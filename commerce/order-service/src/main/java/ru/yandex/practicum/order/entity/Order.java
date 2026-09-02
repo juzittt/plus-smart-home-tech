484105e69284bc63a186fcc83a6e5dfda44ad850
juzittt <juzittt@yandex.ru>
2026-09-02 17:47:57 +0300

    fix: annotation import

--- commerce/order-service/src/main/java/ru/yandex/practicum/order/entity/Order.java
+++ commerce/order-service/src/main/java/ru/yandex/practicum/order/entity/Order.java
@@ -1,6 +1,15 @@
 package ru.yandex.practicum.order.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
--- commerce/order-service/src/main/java/ru/yandex/practicum/order/entity/OrderItem.java
+++ commerce/order-service/src/main/java/ru/yandex/practicum/order/entity/OrderItem.java
@@ -1,10 +1,17 @@
 package ru.yandex.practicum.order.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 
